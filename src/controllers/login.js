38292c63a8656048b76c9035e11367ce470bdf4c
amoodaa <amoodaa@gmail.com>
2019-09-04 20:10:14 +0300

    enabled get and post methods for login and signup with their respective fucntionalities of signing the cookies and adding new users to the db and hashing their password

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,10 +1,18 @@
+const { join } = require('path');
 const { login } = require('../auth/login');
 
-exports.get = (req, res, next) => {};
+exports.get = (req, res) => {
+  if (req.cookies.token) res.redirect('/');
+  else res.sendFile(join(__dirname, '..', '..', 'public', 'html', 'login.html'));
+};
 exports.post = (req, res, next) => {
+  // console.log('req.body:', req.body);
   const { username, password } = req.body;
-  login({ username, password })
-    .then(jwtToken => res.cookie(jwtToken))
+  login({ username, password }, next)
+    .then(jwtToken => res.cookie('token', jwtToken))
     .then(() => res.redirect('/'))
-    .catch(next);
+    .catch(err => {
+      if (err.name === 'TypeError') res.send('user does not exist');
+      else next(err);
+    });
 };
--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -1,16 +1,19 @@
+const { join } = require('path');
 const { hash } = require('bcrypt');
 const validation = require('../validation');
 const { user } = require('../models/queries');
 const { login } = require('../auth/login');
 
 exports.get = (req, res, next) => {
-  // res.
+  if (req.cookies.token) res.redirect('/');
+  else res.sendFile(join(__dirname, '..', '..', 'public', 'html', 'signup.html'));
 };
+
 exports.post = (req, res, next) => {
   const { username, password, confirmPassword, email } = req.body;
 
   validation
-    .signup({ email, username, password, confirmPassword })
+    .signup({ username, password, confirmPassword, email })
     // make sure username is unique errmsg
     .then(() => user.find(username))
     .then(result => {
@@ -18,7 +21,13 @@ exports.post = (req, res, next) => {
     })
     .then(() => hash(password, 10))
     .then(hashed => user.insert({ email, username, password: hashed }))
-    .then(() => login(username, password))
+    .then(() => login({ username, password }))
+    .then(jwtToken => res.cookie('token', jwtToken))
     .then(() => res.redirect('/'))
-    .catch(next);
+    .catch(err => {
+      console.log(err.message);
+      if (err.message === 'username exists') res.status(400).send('username exists');
+      else if (err.message === 'invalid password') res.status(400).send('invalid password');
+      else next(err);
+    });
 };
