3d8aceedd3ac93f181e9f9a6ffddf6470da65200
amoodaa <amoodaa@gmail.com>
2019-09-04 20:06:41 +0300

    modularised login function that returns a signed jwt token

--- src/auth/login.js
+++ src/auth/login.js
@@ -2,12 +2,12 @@ const { sign } = require('jsonwebtoken');
 const { compare } = require('bcrypt');
 const { user } = require('../models/queries');
 /**
- * @returns {string} jwt token
+ * @returns {Promise <string>} jwt token
  *  */
 exports.login = userData => {
   const { username, password } = userData;
   let id;
-  user
+  return user
     .find(username)
     .then(result => result.rows[0])
     .then(foundUser => {
@@ -15,7 +15,7 @@ exports.login = userData => {
       return compare(password, foundUser.password);
     })
     .then(isValidPass => {
-      if (isValidPass) return sign({ id, username });
+      if (isValidPass) return sign({ id, username }, process.env.JWT_KEY);
       throw Error('invalid password');
     });
 };
